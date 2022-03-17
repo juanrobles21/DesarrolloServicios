4035ed218de981120ef472ce94220469af77a16c
juanrobles21 <juan.roblesa@usantoto.edu.co>
2022-03-17 07:56:30 -0500

    actualización de repo

--- dos_007/src/repositorios/usuarios/usuarios_listar_sql.ts
+++ dos_007/src/repositorios/usuarios/usuarios_listar_sql.ts
@@ -1,9 +1,11 @@
 export const SQL_USUARIOS_LISTAR={
-    LISTAR:"SELECT us.nombres_usuario,us.apellidos_usuario,COUNT(ing.cod_ingreso) AS Ingreaso_Sistema \
+    LISTAR:"SELECT us.nombres_usuario,us.apellidos_usuario,ro.nombre_rol,COUNT(ing.cod_ingreso) AS Ingreaso_Sistema \
     FROM usuarios us \
-    INNER JOIN accesos acc \
+    RIGHT JOIN roles ro \
+    ON ro.cod_rol = us.cod_rol\
+    LEFT JOIN accesos acc \
     ON acc.cod_usuario = us.cod_usuario \
-    RIGHT JOIN ingresos ing \
+    INNER JOIN ingresos ing \
     ON ing.cod_usuario = acc.cod_usuario \
-    GROUP BY us.cod_usuario"
+    GROUP BY us.cod_usuario, ro.nombre_rol"
 }
